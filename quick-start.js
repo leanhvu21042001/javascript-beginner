ad76380582fcde31e4bf1177098d968e8d285334
le anh vu <leanhvu@les-MacBook-Pro.local>
2022-08-21 22:01:30 +0700

    quick start

---
+++ quick-start.js
@@ -0,0 +1,9 @@
+/**
+ * Review
+ * How to open Chrome Dev tools
+ * How to put Chrome Dev tools into dark mode
+ * How to use the console tab in Chrome Dev tools to enter JavaScript statements
+ * Four data types: String, Number, Boolean Undefined
+ * How to check data types with the keyword typeof
+ * How to create variable with the keyword let
+ */
